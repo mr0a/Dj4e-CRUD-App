f40b44da381c16784cafd96e9a005523d0808020
Hello <hello@codemy.com>
2020-10-01 11:41:01 +0000

    Basic Auth

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, reverse, include
 from django.views.generic import TemplateView
-from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name = 'crud/main.html')),
+    path('', TemplateView.as_view(template_name = 'crud/main.html'), name = 'main'),
+    # path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
